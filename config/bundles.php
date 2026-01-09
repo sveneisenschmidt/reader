a9108fe0eb6fab4f9b3fc98193727803dca3debb
Sven Eisenschmidt <sveneisenschmidt@users.noreply.github.com>
2026-01-09 17:37:19 +0100

    Revert minify-bundle, restore phpunit coverage config

--- config/bundles.php
+++ config/bundles.php
@@ -1,20 +1,14 @@
 <?php
 
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ["all" => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ["all" => true],
-    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ["all" => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ["all" => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => [
-        "all" => true,
-    ],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => [
-        "dev" => true,
-        "test" => true,
-    ],
-    Knp\Bundle\TimeBundle\KnpTimeBundle::class => ["all" => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ["all" => true],
-    Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ["all" => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ["all" => true],
-    Sensiolabs\MinifyBundle\SensiolabsMinifyBundle::class => ["all" => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
