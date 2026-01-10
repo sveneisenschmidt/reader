df41535511ab45ccaab65db57f099aa7ff869fc2
Sven Eisenschmidt <sveneisenschmidt@users.noreply.github.com>
2026-01-10 13:43:07 +0100

    Enable parallel php-cs-fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,11 +3,15 @@
 $finder = new PhpCsFixer\Finder()
     ->in(__DIR__)
     ->exclude("var")
-    ->exclude("vendor");
+    ->exclude("vendor")
+    ->exclude("node_modules");
 
 return new PhpCsFixer\Config()
     ->setRules([
         "@Symfony" => true,
         "yoda_style" => false,
     ])
+    ->setParallelConfig(
+        PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect(),
+    )
     ->setFinder($finder);
