819413d535b3c0e455a1d7f6d0b6f8acdcb1d1cb
Sven Eisenschmidt <sveneisenschmidt@users.noreply.github.com>
2026-01-12 14:50:08 +0100

    Update screenshots and dev user

--- fixtures/dev-user.sql
+++ fixtures/dev-user.sql
@@ -3,8 +3,8 @@ INSERT INTO user (username, email, password, totp_secret, theme, created_at)
 VALUES (
     'dev@localhost.arpa',
     'dev@localhost.arpa',
-    '$2y$13$8wsRpG.JyrTHB1ORc63lMOjNF58JrbnuwYj5h919Qv8Oe6OvzXI7y',
-    '6i4IwE1zqA0kW447uWm2Anq+vap+Utck3tR+dmgCriCCVKXRZSvHO2SBqGlcHuV+XFq4y+q/wK8OH8KU8CfGEmpKpr+5yKcCxhHkzoRAsi3YRWdPJ1cX/NeZzy0iNjwoUjVCtjHNWPjAOOQLML/4iAUCK7HWj7qhSYdGj8l63WLzTSaM0vnN/cxvs19rfFo=',
+    '$2y$13$LjFHqJbDMLa9ilMaPf8lMOljUOc87tiXBQ3a16tLm6kA5skCyuAoC',
+    '6Q0VHyiP1F5li7Ahb9q1b9Gosp3ZUAa0xrgn+3ra/M+EQ9dU7+7zOEAi20J4m9HJUd4RrlwPWgaQ7HfCTgjfGp7MHAsKOBPrMXrWW8VcoYoS8HFQhVmSLW/yLBZAjvwsOLg0aANdsqgltCZRdzRTjbiH8E5fROuP9SjVXVzYNeIRhxeAVh1s9+9+PTtejUg=',
     'auto',
     datetime('now')
 );
