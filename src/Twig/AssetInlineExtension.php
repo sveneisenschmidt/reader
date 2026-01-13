881381cbd96ac50da24418526e8bbb7b11d045e2
Sven Eisenschmidt <sveneisenschmidt@users.noreply.github.com>
2026-01-13 18:36:31 +0100

    Use asset() in error templates, remove preload block, fix asset_inline null return

--- src/Twig/AssetInlineExtension.php
+++ src/Twig/AssetInlineExtension.php
@@ -19,16 +19,15 @@ class AssetInlineExtension extends AbstractExtension
     public function __construct(
         private AssetMapperInterface $assetMapper,
         private string $publicDir,
-    ) {
-    }
+    ) {}
 
     public function getFunctions(): array
     {
         return [
             new TwigFunction(
-                'asset_inline',
-                [$this, 'assetInline'],
-                ['is_safe' => ['html']],
+                "asset_inline",
+                [$this, "assetInline"],
+                ["is_safe" => ["html"]],
             ),
         ];
     }
@@ -38,17 +37,21 @@ class AssetInlineExtension extends AbstractExtension
         $asset = $this->assetMapper->getAsset($path);
 
         if ($asset === null) {
-            return '';
+            return "";
         }
 
         // Try compiled file in public/ first, then fall back to source
-        $publicFile = $this->publicDir.$asset->publicPath;
+        $publicFile = $this->publicDir . $asset->publicPath;
         if (file_exists($publicFile)) {
             $content = file_get_contents($publicFile);
         } else {
             $content = $asset->content ?? file_get_contents($asset->sourcePath);
         }
 
-        return '/* '.$asset->publicPath." */\n".$content;
+        if ($content === false) {
+            return "";
+        }
+
+        return "/* " . $asset->publicPath . " */\n" . $content;
     }
 }
