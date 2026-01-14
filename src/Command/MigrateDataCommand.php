8d681dcc4b2e1f75c16f6abc83a6b123c18c3a38
Sven Eisenschmidt <sveneisenschmidt@users.noreply.github.com>
2026-01-14 08:10:10 +0100

    Add CodeCoverageIgnore to MigrateDataCommand

--- src/Command/MigrateDataCommand.php
+++ src/Command/MigrateDataCommand.php
@@ -20,10 +20,11 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[
     AsCommand(
-        name: 'reader:migrate-data',
-        description: 'Migrate data from old multi-database structure to single database',
+        name: "reader:migrate-data",
+        description: "Migrate data from old multi-database structure to single database",
     ),
 ]
+#[\CodeCoverageIgnore]
 class MigrateDataCommand extends Command
 {
     public function __construct(
@@ -35,34 +36,33 @@ class MigrateDataCommand extends Command
 
     protected function configure(): void
     {
-        $this
+        $this->addOption(
+            "users-db",
+            null,
+            InputOption::VALUE_REQUIRED,
+            "Path to old users database",
+            "var/data/users.db",
+        )
             ->addOption(
-                'users-db',
+                "subscriptions-db",
                 null,
                 InputOption::VALUE_REQUIRED,
-                'Path to old users database',
-                'var/data/users.db',
+                "Path to old subscriptions database",
+                "var/data/subscriptions.db",
             )
             ->addOption(
-                'subscriptions-db',
+                "content-db",
                 null,
                 InputOption::VALUE_REQUIRED,
-                'Path to old subscriptions database',
-                'var/data/subscriptions.db',
+                "Path to old content database",
+                "var/data/content.db",
             )
             ->addOption(
-                'content-db',
+                "messages-db",
                 null,
                 InputOption::VALUE_REQUIRED,
-                'Path to old content database',
-                'var/data/content.db',
-            )
-            ->addOption(
-                'messages-db',
-                null,
-                InputOption::VALUE_REQUIRED,
-                'Path to old messages database',
-                'var/data/messages.db',
+                "Path to old messages database",
+                "var/data/messages.db",
             );
     }
 
@@ -72,10 +72,12 @@ class MigrateDataCommand extends Command
     ): int {
         $io = new SymfonyStyle($input, $output);
 
-        $usersDb = $this->resolvePath($input->getOption('users-db'));
-        $subscriptionsDb = $this->resolvePath($input->getOption('subscriptions-db'));
-        $contentDb = $this->resolvePath($input->getOption('content-db'));
-        $messagesDb = $this->resolvePath($input->getOption('messages-db'));
+        $usersDb = $this->resolvePath($input->getOption("users-db"));
+        $subscriptionsDb = $this->resolvePath(
+            $input->getOption("subscriptions-db"),
+        );
+        $contentDb = $this->resolvePath($input->getOption("content-db"));
+        $messagesDb = $this->resolvePath($input->getOption("messages-db"));
 
         // Verify old databases exist
         $missingDbs = [];
@@ -93,41 +95,45 @@ class MigrateDataCommand extends Command
         }
 
         if (!empty($missingDbs)) {
-            $io->error('Missing database files: '.implode(', ', $missingDbs));
+            $io->error("Missing database files: " . implode(", ", $missingDbs));
 
             return Command::FAILURE;
         }
 
-        $io->title('Migrating data from old databases');
+        $io->title("Migrating data from old databases");
 
         // Migrate users database (user, user_preference, read_status, seen_status)
-        $io->section('Migrating users database');
-        $this->migrateDatabase($usersDb, 'old_users', [
-            'user',
-            'user_preference',
-            'read_status',
-            'seen_status',
-        ], $io);
+        $io->section("Migrating users database");
+        $this->migrateDatabase(
+            $usersDb,
+            "old_users",
+            ["user", "user_preference", "read_status", "seen_status"],
+            $io,
+        );
 
         // Migrate subscriptions database
-        $io->section('Migrating subscriptions database');
-        $this->migrateDatabase($subscriptionsDb, 'old_subs', [
-            'subscription',
-        ], $io);
+        $io->section("Migrating subscriptions database");
+        $this->migrateDatabase(
+            $subscriptionsDb,
+            "old_subs",
+            ["subscription"],
+            $io,
+        );
 
         // Migrate content database
-        $io->section('Migrating content database');
-        $this->migrateDatabase($contentDb, 'old_content', [
-            'feed_item',
-        ], $io);
+        $io->section("Migrating content database");
+        $this->migrateDatabase($contentDb, "old_content", ["feed_item"], $io);
 
         // Migrate messages database
-        $io->section('Migrating messages database');
-        $this->migrateDatabase($messagesDb, 'old_msgs', [
-            'processed_message',
-        ], $io);
+        $io->section("Migrating messages database");
+        $this->migrateDatabase(
+            $messagesDb,
+            "old_msgs",
+            ["processed_message"],
+            $io,
+        );
 
-        $io->success('Data migration completed successfully');
+        $io->success("Data migration completed successfully");
 
         return Command::SUCCESS;
     }
@@ -147,27 +153,32 @@ class MigrateDataCommand extends Command
 
         foreach ($tables as $table) {
             $count = $this->connection->fetchOne(
-                sprintf('SELECT COUNT(*) FROM %s.%s', $alias, $table),
+                sprintf("SELECT COUNT(*) FROM %s.%s", $alias, $table),
             );
 
             $this->connection->executeStatement(
-                sprintf('INSERT INTO %s SELECT * FROM %s.%s', $table, $alias, $table),
+                sprintf(
+                    "INSERT INTO %s SELECT * FROM %s.%s",
+                    $table,
+                    $alias,
+                    $table,
+                ),
             );
 
-            $io->writeln(sprintf('  Migrated %d rows from %s', $count, $table));
+            $io->writeln(sprintf("  Migrated %d rows from %s", $count, $table));
         }
 
         $this->connection->executeStatement(
-            sprintf('DETACH DATABASE %s', $alias),
+            sprintf("DETACH DATABASE %s", $alias),
         );
     }
 
     private function resolvePath(string $path): string
     {
-        if (str_starts_with($path, '/')) {
+        if (str_starts_with($path, "/")) {
             return $path;
         }
 
-        return $this->projectDir.'/'.$path;
+        return $this->projectDir . "/" . $path;
     }
 }
