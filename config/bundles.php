c43fb3b69f6aa695d376a8d88046032eb06938da
Sven Eisenschmidt <sveneisenschmidt@users.noreply.github.com>
2026-01-14 07:04:10 +0100

    Enable WebProfilerBundle for all environments

--- config/bundles.php
+++ config/bundles.php
@@ -5,8 +5,12 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => [
+        'all' => true,
+    ],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => [
+        'all' => true,
+    ],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ['all' => true],
